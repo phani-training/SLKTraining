214965a5bc404c9dd1bfa8608639e05ab755986e
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-07-19 10:17:33 +0530

    Add files via upload

---
+++ SlkTraining/SampleConApp/Day2/Ex02ControlStatements.cs
@@ -0,0 +1,64 @@
+﻿using System;
+//Loops, If Statements, Switch Statements.
+//For loop, Foreach Loop, While and Do...While.
+namespace SampleConApp.Day2
+{
+    class Ex02ControlStatements
+    {
+        static void DoWhileExample()
+        {
+            bool processing = false;
+            do
+            {
+                Console.WriteLine("Select the option as 1, 2 or 3");
+                int option = int.Parse(Console.ReadLine());
+                processing = SwitchCaseExample(option);
+            } while (processing);
+        }
+        //static void SwitchCaseExample(int option)
+        //{
+        //        Console.WriteLine("Select the option as 1, 2 or 3");
+        //        int option = int.Parse(Console.ReadLine());
+        //    switch (option)
+        //    {
+        //        case 1: Console.WriteLine("User selected the First menu item"); break;
+        //        case 2: Console.WriteLine("User selected the Mid menu item"); break;
+        //        case 3: Console.WriteLine("User selected the Last menu item"); break;
+        //        default: Console.WriteLine("User selected an invalid Choice"); break;
+        //    }
+        //}
+
+        static bool SwitchCaseExample(int option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("User selected the First menu item"); break;
+                case 2: Console.WriteLine("User selected the Mid menu item"); break;
+                case 3: Console.WriteLine("User selected the Last menu item"); break;
+                default: Console.WriteLine("User selected an invalid Choice"); return false;
+            }
+            return true;
+        }
+        static void IfConditionExample()
+        {
+            Console.WriteLine("What Do U want to learn today?");
+            string answer = Console.ReadLine();
+            if (answer == ".NET")//Looks for Boolean Expression, trueness will execute the block. 
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("I dont do " + answer);
+            }
+            //if...else
+            //if...elseif...elseif...else
+        }
+        static void Main(string[] args)
+        {
+            IfConditionExample();
+            //SwitchCaseExample();
+            DoWhileExample();
+        }
+    }
+}
---
+++ SlkTraining/SampleConApp/Day2/Ex03AccessModifiersDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+/*
+ * public(Accessible to all), 
+ * private(Only within the declared class), 
+ * protected(Only within the declared class and its derived Class, 
+ * internal(Only within the Project(Assembly))
+ * default access specifier for the Project level is internal
+ * default access specifier within the class is private. 
+ * */
+namespace SampleConApp.Day2
+{
+    //Members within the class are default private to the class. The members are inaccessible to the Users outside the class. Accessors will be 2 types: methods and properties.  
+    class Person//Internal to the Project
+    {
+        long adhaarNo;
+        string personName;
+        string personPhone;
+        string personAddress;
+
+        public void SetAaddharNo(long no)
+        {
+            adhaarNo = no;
+        }
+
+        public long GetAadharNo()
+        {
+            return adhaarNo;
+        }
+    }
+
+    class Ex03AccessModifiersDemo
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.SetAaddharNo(123123131231);
+            Console.WriteLine("The no is " + person.GetAadharNo());
+        }
+    }
+}
