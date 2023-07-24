0da69effc48f560875953ddb47fefeb3ab2ecc93
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-07-24 10:04:23 +0530

    Day 4 Content of Inheritance and Polymorphism

---
+++ SlkTraining/SampleConApp/Day4/Ex01Inheritance.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Inheritance is a feature of OOP based on a principle called open-closed principle. A Class is open for Extension but closed for modification.
+ * A class once created, is immutable. U cannot change the class. 
+ * Any change that U wish to implement to a class has to be done thru extension where the class is inherited into another and that class will be provided with these additional changes. 
+ * SOLID Principles of OOP.
+ * The class that is being extended is class as BASE/SUPER/PARENT class. The class that is extending is called as DERIVED/SUB/CHILD class. 
+ * C# supports Single Inheritance. A class at any given level can have only one base class. C++ supports multiple Inheritance which has got issues with hirarchy. However, it supports multi level inheritance. A class can have a base class that is further derived from another class and so forth.  
+ * There is no public accessor while U derive from the base class. All base class methods are available with the Same access specifier to its derived classes. 
+ */
+namespace SampleConApp.Day4
+{
+
+    class Account
+    {
+        public int AccountNo { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+    }
+
+    class SBAccount : Account
+    {
+        public double RateOfInterest { get; set; }
+
+        public void CreditAmount(double amount) => Balance += amount;
+
+        public void DebitAmount(double amount)
+        {
+            if(Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                throw new Exception("Insufficient Funds");
+            }
+        }
+
+        public void CalculateInterest()
+        {
+            double principle = Balance;
+            double term = 0.25;
+            double interest = (principle * RateOfInterest * term) / 100;
+            CreditAmount(interest);
+        }
+    }
+    class Ex01Inheritance
+    {
+        static void Main()
+        {
+            Account acc = new Account { AccountNo = 111, Balance = 6000, Name = "Phaniraj" };
+
+            SBAccount sbAcc = new SBAccount { AccountNo = 111, Balance = 6000, Name = "Phaniraj", RateOfInterest = 6.5 };
+            sbAcc.DebitAmount(399);
+            sbAcc.CreditAmount(60000);
+            sbAcc.CalculateInterest();
+            Console.WriteLine("The Balance is " + sbAcc.Balance);
+        }
+    }
+}
---
+++ SlkTraining/SampleConApp/Day4/Ex02Polymorphism.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Polymorphism is a feature of OOP that allows a member to behave in different manner based on the parameters or the object to which it belongs. With Parameters change, it is called Compile Time Polymorphism and with Object instantiation, its called as RUNTIME Polymorphism
+ * POLY means many Morphism means kinds. 
+ * If a class has to modify an existing method of the base class, it is called as METHOD OVERRIDING. In this case, the method signature(return type, name, parameters) should be retained in the derived version. The base method should be having a modifier 'virtual'. The Virtual methods are reimplemented(Overriden) using a modifier called override. Only virtual methods can be overriden in the derived classes. 
+ * If a method is not virtual in the base class, it cannot be overriden. however U can reimplement the method in the derived class, but it will not behave polymorphically. 
+ * Create a class called GeometricOperations with static methods to find area of Rectangle, Circle, Triangle, where the method name is simply GetArea and variable no of args: 
+ * Rectange: b*h(2 parameters)
+ * Circle: r(1 Parameter)
+ * Triangle:3 Parameters, int, int, string("Triangle")  
+ */
+
+namespace PolymorphismDemo
+{
+    class BaseClass
+    {
+        public virtual void TestFunc() => Console.WriteLine("Test Func is called");
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public override void TestFunc() => Console.WriteLine("Test Func in a new Manner");
+    }
+}
+namespace SampleConApp.Day4
+{
+    using PolymorphismDemo;
+
+
+    class ClassFactory
+    {
+        public static BaseClass GetObject(string type)
+        {
+            if (type.ToUpper() == "BASE")
+                return new BaseClass();
+            else if (type.ToUpper() == "DERIVED")
+                return new DerivedClass();
+            else
+                throw new Exception("Invalid Class type");
+        }
+    }
+    class Ex02Polymorphism
+    {
+        static void Main()
+        {
+            Console.WriteLine("Which class U want to implement: Base or Derived");
+            string answer = Console.ReadLine();
+
+            BaseClass cls = ClassFactory.GetObject(answer);
+            cls.TestFunc();
+        }
+    }
+}
