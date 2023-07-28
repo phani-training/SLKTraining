69fe865ccc72b2b432be4105596548ab09ef1f8a
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-07-28 10:03:34 +0530

    Add files via upload

---
+++ SlkTraining/SampleConApp/Day6/Ex01StructsExample.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+* Structs are value types in .NET
+* They store the value instead of memory location(Reference)
+* All Value types are structs in C#.
+* Integral Types: byte, short, int and long
+* Floating types: float(Single), double(Double), decimal(Decimal)
+* Other types:char, bool, DateTime. 
+* User defined types: Enums and structs. 
+* Minified version of a class. With struct, U can have UR data, functions for the data. But it cannot be OO feature. 
+* Constructors can be only parameterized constructors.
+* U cannot have fields or properties initialized while declaring it. 
+*/
+namespace SampleConApp.Day6
+{
+    interface IEmpComponent
+    {
+        void AddNewEmployee(Employee emp);
+        void UpdateEmployee(Employee emp);
+        void DeleteEmployee(int empId);
+        List<Employee> GetAllEmployees();
+    }
+
+    struct EmployeeManager : IEmpComponent
+    {
+        private List<Employee> empList { get; set; }
+
+        public EmployeeManager(int size)//Will not use size, it is created to solve the parameterless ctor issue
+        {
+            empList = new List<Employee>(0);
+        }
+        public void AddNewEmployee(Employee emp)
+        {
+                empList.Add(emp);  
+        }
+
+        public void DeleteEmployee(int empId)
+        {
+            foreach(Employee emp in empList)
+            {
+                if(emp.EmpId == empId)
+                {
+                    empList.Remove(emp);
+                    return;
+                }
+            }
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return empList;
+        }
+
+        public void UpdateEmployee(Employee updatedEmp)
+        {
+            for (int i = 0; i < empList.Count; i++)
+            {
+                var temp = empList[i];
+                if(empList[i].EmpId == updatedEmp.EmpId)
+                {
+                    temp.EmpName = updatedEmp.EmpName;
+                    temp.EmpName = updatedEmp.EmpAddress;
+                    temp.EmpSalary = updatedEmp.EmpSalary;
+                    empList[i] = temp;
+                    return;
+                }
+            }
+        }
+    }
+    struct Employee
+    {
+        public int EmpSalary { get; set; }
+        public string EmpAddress { get; set; }
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+       
+       public void Display()
+        {
+            Console.WriteLine($"------------Details of Mr.{EmpName.ToUpper()}----------------");
+            Console.WriteLine("The ID: " + EmpId);
+            Console.WriteLine("The Name: " + EmpName);
+            Console.WriteLine("The Address: " + EmpAddress);
+            Console.WriteLine("The Salary: {0:c}",EmpSalary);
+            Console.WriteLine("--------------------------------------------------------------");
+        }
+    }
+
+   //Use  this code and improvise the Application with the feature of a menu driven program that will have options like add, remove,find, update etc. The code should not abnormally terminate. If the User wants to close the Application, U should provide an option to close. Data is not stored to any external storage device. 
+    class Ex01StructsExample
+    {
+        static void Main(string[] args)
+        {
+            //Like Class instantiation
+            var emp = new Employee { EmpId = 123, EmpName = "Phaniraj", EmpAddress = "Bangalore", EmpSalary = 56000 };
+            //emp.Display();
+            try
+            {
+                EmployeeManager empMgr = new EmployeeManager(1000000);
+                empMgr.AddNewEmployee(emp);
+                Console.WriteLine("Employee added successfully");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SlkTraining/SampleConApp/Day6/Ex02ExceptionHandling.cs
@@ -0,0 +1,56 @@
+﻿using System;
+/*
+ * Exceptions are abnormal termination of a program when a certain logic is missed. The programmer or the application does not intend to move further because of a wrong input or the app fails to understand on how to go forward.
+ * All Exceptions are objects of a class derived from System.Exception. 
+ * Exceptions are generated when the program encounters a problem, at that time, the Runtime will create an object of class derived from System.Exception and throw that exception into the Application process.
+ * Developers should handle the exception with try..catch..finally block
+ * Here try should be followed by either catch or finally or both. 
+ * finally is a block that is executed on all conditions.
+ * catch blocks are executed as per the Exception class that is used to handle. 
+ */
+
+namespace SampleConApp.Day6
+{
+    //Custom exception is a class derived from Exception where we create to define Application specific Exceptions. 
+    public class AgeException : Exception
+    {
+        public AgeException() { }
+        public AgeException(string message) : base(message) { }
+        public AgeException(string message, Exception inner) : base(message, inner) { }
+       
+    }
+    class Ex02ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            RETRY:
+            Console.WriteLine("Enter the age");
+            try
+            {
+                uint age = uint.Parse(Console.ReadLine());
+                if (age == 0)
+                    throw new AgeException("Age should not be 0 or negative");
+                Console.WriteLine("The Age is " + age);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The input is expected to be a whole number");
+                goto RETRY;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"It should be b/w {uint.MinValue} and {uint.MaxValue}");
+                goto RETRY;
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto RETRY;
+            }
+            finally
+            {
+                Console.WriteLine("End of the Program");
+            }
+        }
+    }
+}
