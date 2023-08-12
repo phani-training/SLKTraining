c2c9ac285ec2793cfb130291c34000f13518b253
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-08-12 10:45:21 +0530

    Updated with Stored prc

--- SlkTraining/SampleConApp/Day10/Ex01DataAccess.cs
+++ SlkTraining/SampleConApp/Day10/Ex01DataAccess.cs
@@ -11,12 +11,38 @@ namespace SampleConApp.Day10
     {
         const string CONNECTIONSTRING = @"Data Source=.\SQLEXPRESS;Initial Catalog=SLKDatabase;Integrated Security=True";
         const string GETALL = "SELECT * FROM EMPTABLE";
+        const string STRINSERTPROCNAME = "createEmp";
         static void Main(string[] args)
         {
             //readRecords();
             //insertRecord(116, "Jullian", "Jullian@gmail.com", 987969896, 50000);
+            insertRecordByStoredProc();
         }
+        private static void insertRecordByStoredProc()
+        {
+            var con = new SqlConnection(CONNECTIONSTRING);
+            var cmd = new SqlCommand(STRINSERTPROCNAME, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", "Banu Prakash");
+            cmd.Parameters.AddWithValue("@address", "Bangalore");
+            cmd.Parameters.AddWithValue("@email", "banucprakash@gmail.com");
+            cmd.Parameters.AddWithValue("@salary", 56000);
+            cmd.Parameters.AddWithValue("@dept", 4);
+            try
+            {
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if(rowsAffected == 1)
+                {
+                    Console.WriteLine("The employee is inserted successfully");
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
+
+        }
         private static void insertRecord(int id, string name, string email, long phone, int salary)
         {
             string insertStatement = $"Insert into EmpTable values({id}, '{name}', '{email}', {phone}, {salary})";
